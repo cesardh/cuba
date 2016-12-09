cf38706fb76587122f93f0227854a3add18c8bd2
César Duarte Higuita <cesardh777@gmail.com>
2016-12-09 00:18:44 -0500

    transición

--- js/script.js
+++ js/script.js
@@ -1,12 +1,17 @@
 $(document).ready(function() {
   var $capBtn = $('nav>ul>li')
   var $pr = $('#principal')
+  var $sec = $('.seccion')
+
   $capBtn.click(function(e) {
+
     if(this == $capBtn[0]){
-      $pr.scrollTop(0)
+      $($sec[0]).slideDown(1500)
+      $($sec[1]).slideUp(2000)
     }else if (this == $capBtn[1]) {
-      var w = $(window).height()
-      $pr.scrollTop(w)
+      $($sec[1]).slideDown(1500)
+      $($sec[0]).slideUp(2000)
     }
+
   });
 });
