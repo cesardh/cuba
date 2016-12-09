2a55a90919e7d48722be11fd512f4c4899052c54
César Duarte Higuita <cesardh777@gmail.com>
2016-12-09 13:29:21 -0500

    09/12/16 1pm

--- js/script.js
+++ js/script.js
@@ -6,12 +6,25 @@ $(document).ready(function() {
   $capBtn.click(function(e) {
 
     if(this == $capBtn[0]){
-      $($sec[0]).slideDown(1500)
-      $($sec[1]).slideUp(2000)
+      $($sec[0]).stop().slideDown(1500)
+      $($sec[1]).stop().slideUp(2000)
     }else if (this == $capBtn[1]) {
-      $($sec[1]).slideDown(1500)
-      $($sec[0]).slideUp(2000)
+      $($sec[1]).stop().slideDown(1500)
+      $($sec[0]).stop().slideUp(2000)
     }
 
   });
+
+  // $("#cap1").scroll(function(){
+  // 	var windowHeight = $('.seccion').scrollTop();
+  // 	var contenido2 = $(".pruebascroll").offset();
+  //   console.log(contenido2, windowHeight);
+  // 	contenido2 = contenido2.top;
+  //
+  // 	if(windowHeight >= contenido2  ){
+  // 	   console.log(contenido2)
+  // 	}else{
+  //     console.log("scroll no está sobre imágen")
+  // 	}
+	// });
 });
