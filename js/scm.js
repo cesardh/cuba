66ca88a6e31ab7d3820b58356ec4e642a1a34afc
César Duarte Higuita <cesardh777@gmail.com>
2016-12-28 00:54:24 -0500

    SVG

--- js/scm.js
+++ js/scm.js
@@ -2,10 +2,5 @@ var control = new ScrollMagic.Controller({container: "#cap1"});
 
 var scene = new ScrollMagic.Scene({triggerElement: "#trig", duration: 200})
 					// animate color and top border in relation to scroll position
-					.setTween("img:first", {opacity: 0})
-					.addTo(control);
-
-var scene2 = new ScrollMagic.Scene({triggerElement: "#trig", duration: 300})
-					// animate color and top border in relation to scroll position
-					.setTween("#pin", {transform: "translateX(100px)"})
+					.setTween("img:first", {filter: "brigthness(0%)"})
 					.addTo(control);
