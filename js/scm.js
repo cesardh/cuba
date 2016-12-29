30b198c74134a7a737de88039cfa31a6e80ad8bf
César Duarte Higuita <cesardh777@gmail.com>
2016-12-29 11:17:10 -0500

    secciones

--- js/scm.js
+++ js/scm.js
@@ -1,6 +1,17 @@
 var control = new ScrollMagic.Controller({container: "#cap1"});
 
-var scene = new ScrollMagic.Scene({triggerElement: "#trig", duration: 200})
-					// animate color and top border in relation to scroll position
-					.setTween("img:first", {filter: "brigthness(0%)"})
-					.addTo(control);
+new ScrollMagic.Scene({triggerElement: "#cap1-p1", duration: "100%", triggerHook: "onLeave"})
+	.setTween("#cap1 .capitulo", {opacity: 0})
+	.addTo(control);
+
+new ScrollMagic.Scene({triggerElement: ".separar", duration: 300, triggerHook: "onEnter"})
+	.setTween("#cap1-p1 p", {opacity: 1, transform: "scale(1) rotateY(0grad) translateX(0px)"})
+	.addTo(control);
+
+new ScrollMagic.Scene({triggerElement: "#cap1-p2", duration: "150%", triggerHook: "onEnter"})
+	.setTween("#avion", {transform: "translateX(100vw)"})
+	.addTo(control);
+
+new ScrollMagic.Scene({triggerElement: "#cap1-p2", duration: 400, triggerHook: "onLeave"})
+	.setTween("#cap1-p2 p:nth-child(3)", {opacity: 1, transform: "translatey(00px) rotateX(0grad)"})
+	.addTo(control);
--- js/script.js
+++ js/script.js
@@ -12,25 +12,13 @@
 $(document).ready(function() {
   var $capBtn = $('nav>ul>li')
   var $sec = $('#principal>.seccion')
-  console.log($sec);
 
   $capBtn.click(function(e) {
     var index = $capBtn.index(this);
-    $(".activo").stop().slideUp(2000).toggleClass('activo');
+    $capBtn.removeClass('activo')
+    $(this).addClass('activo')
+    $(".seccion.activo").stop().slideUp(2000).toggleClass('activo');
     $($sec[index]).stop().slideDown(2000).toggleClass('activo');
 
   });
-
-  // $("#cap1").scroll(function(){
-  // 	var windowHeight = $('.seccion').scrollTop();
-  // 	var contenido2 = $(".pruebascroll").offset();
-  //   console.log(contenido2, windowHeight);
-  // 	contenido2 = contenido2.top;
-  //
-  // 	if(windowHeight >= contenido2  ){
-  // 	   console.log(contenido2)
-  // 	}else{
-  //     console.log("scroll no está sobre imágen")
-  // 	}
-	// });
 });
