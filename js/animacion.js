7aeae75e002ee10775f589462dd1f096da4e28a8
César Duarte Higuita <cesardh777@gmail.com>
2017-01-05 15:29:39 -0500

    cap1

---
+++ js/animacion.js
@@ -0,0 +1,31 @@
+var control = new ScrollMagic.Controller({container: "#cap1"});
+
+new ScrollMagic.Scene({
+	triggerElement: "#cap1-p1", duration: "70%", triggerHook: "onLeave"
+})
+.setTween("#cap1 .capitulo", {opacity: 0})
+.addTo(control);
+
+new ScrollMagic.Scene({
+	triggerElement: ".separar", duration: 300, triggerHook: "onEnter"
+})
+.setTween("#cap1-p1 p", {opacity: 1, transform: "scale(1) rotateY(0grad)"})
+.addTo(control);
+
+new ScrollMagic.Scene({
+	triggerElement: "#cap1-p2", duration: "150%", triggerHook: "onEnter"
+})
+.setTween("#avion", {transform: "translateX(120vw)"})
+.addTo(control);
+
+new ScrollMagic.Scene({
+	triggerElement: "#retratos", duration: 300, triggerHook: 0.7
+})
+.setTween(".t2", {opacity: 1, transform: "translatey(0px) rotateX(0grad)"})
+.addTo(control);
+
+new ScrollMagic.Scene({
+	triggerElement: ".t1", triggerHook: 0.2
+})
+.setClassToggle("#retratos div", "anim")
+.addTo(control);
--- js/script.js
+++ js/script.js
@@ -14,17 +14,58 @@ $(document).ready(function() {
   $(window).load(function(){
     $("#load").delay(1000).fadeOut(1000);
   });
-  
-  var $capBtn = $('nav>ul>li')
-  var $sec = $('#principal>.seccion')
+
+  $(".seccion").niceScroll({
+    scrollspeed: 200, mousescrollstep: 20, horizrailenabled: false
+  });
+
+  var $capBtn = $('nav>ul>li');
+  var $sec = $('#principal>.seccion');
+  var enAnim = false;
 
   $capBtn.click(function(e) {
-    var index = $capBtn.index(this);
-    $capBtn.removeClass('activo')
-    $(this).addClass('activo')
-    $(".seccion.activo").stop().slideUp(2000).toggleClass('activo');
-    $($sec[index]).stop().slideDown(2000).toggleClass('activo');
-
+    if(!enAnim && !$(this).hasClass('activo'))
+    {
+      enAnim = true
+      var index = $capBtn.index(this);
+      $capBtn.removeClass('activo');
+      $(this).addClass('activo');
+      $(".seccion.activo").stop().slideUp(2000).toggleClass('activo');
+      $($sec[index]).stop().slideDown(2000, function(){enAnim=false}).toggleClass('activo');
+    //   $(".seccion.activo")
+    //     .toggleClass('activo')
+    //     .velocity({
+    //       scaleX: 0,
+    //       opacity: 0
+    //     }, {
+    //       duration: 2000,
+    //       display: "none"
+    //     });
+    //
+    //   $("#cap"+index+"")
+    //     .velocity({
+    //       scaleX: 1,
+    //       opacity: 1
+    //     }, {
+    //       duration: 2000,
+    //       display: "block",
+    //       complete: function(){enAnim=false}
+    //     })
+    //     .toggleClass('activo');
+    }
   });
+
+  $("#retratos div").click(function(event) {
+    $(this).find('p').css({
+      opacity: 1,
+      top: "0%"
+    });
+  })
+  .mouseleave(function(event) {
+    $(this).find('p').css({
+      opacity: 0,
+      top: "-100%"
+    });
+  });;
 
 });
